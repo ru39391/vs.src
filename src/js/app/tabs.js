f64ea15a2ba87c22c342f0b66c1963d49db9b60b
ru39391 <dev@39391.ru>
2022-12-24 01:11:32 +0300

    feature/настроить переключатели выбора абонемента

--- src/js/app/tabs.js
+++ src/js/app/tabs.js
@@ -1,12 +1,25 @@
 import { Tablist } from '../components/Tablist';
+import { TablistWithForm } from '../components/TablistWithForm';
 
 const tabListConfig = {
-  tabTogglerSel: '.tab-toggler',
-  tabPaneSel: '.tab-pane',
-  tabPaneClassActive: 'active',
+  tabTogglerSel: '.section__tab-toggler',
+  tabPaneSel: '.section__tab',
+  tabPaneClassActive: 'section__tab_active',
 };
-const tabListsArr = Array.from(document.querySelectorAll('.tab-list'));
+const tabListsArr = Array.from(document.querySelectorAll('.section__tablist'));
 tabListsArr.forEach((tabListsArrEl) => {
   const tablist = new Tablist(tabListsArrEl, tabListConfig);
+  tablist.setEventListeners();
+});
+
+const formTabListConfig = {
+  tabTogglerSel: '.product-tablist__toggler',
+  tabPaneSel: '.product-tablist__pane',
+  tabPaneClassActive: 'product-tablist__pane_active',
+};
+const formTabListsArr = Array.from(document.querySelectorAll('.section__tablist'));
+formTabListsArr.forEach((formTabListsArrEl) => {
+  const tablist = new TablistWithForm(formTabListsArrEl, formTabListConfig);
+  tablist.init();
   tablist.setEventListeners();
 });
--- src/js/main.js
+++ src/js/main.js
@@ -9,31 +9,6 @@ import './app/header';
 import './app/dropdown';
 import './app/selecter';
 
-let boxArr = [];
-document.querySelectorAll('.ms2_product[id*="size"]').forEach(boxElem => {
-    boxArr.push(boxElem);
-});
-document.querySelectorAll('.form__toggler[name="size"]').forEach(sizeToggler => {
-    sizeToggler.addEventListener('change', (e) => {
-        boxArr.forEach(boxArrElem => {
-            boxArrElem.classList.add('d-none');
-        });
-        switch(e.target.checked) {
-            case true:
-            if(document.querySelector(`.ms2_product[id="${ e.target.getAttribute('value') }"]`).classList.contains('d-none')) {
-                document.querySelector(`.ms2_product[id="${ e.target.getAttribute('value') }"]`).classList.remove('d-none');
-            }
-            break;
-
-            case false:
-            if(document.querySelector(`.ms2_product[id="${ e.target.getAttribute('value') }"]`).classList.contains('d-none')) {
-                document.querySelector(`.ms2_product[id="${ e.target.getAttribute('value') }"]`).classList.add('d-none');
-            }
-            break;
-        }
-    })
-});
-
 /* auth */
 if(document.querySelector('#office-auth-recovery-username')) {
     document.querySelector('#office-auth-recovery-username').addEventListener('keyup', (e) => {
